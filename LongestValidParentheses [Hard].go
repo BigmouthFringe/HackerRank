7a3fb1aca5646709eb17202ccb853345b30bf204
BigmouthFringe <bigmouthfringe@gmail.com>
2019-01-20 17:49:37 +0700

    Modify algorithm with recursive approach, fix most of testcases

--- LongestValidParentheses [Hard].go
+++ LongestValidParentheses [Hard].go
@@ -2,135 +2,68 @@ package main
 
 import "fmt"
 
-// Valid parentheses pair:
-// Begins with '('
-// Ends with ')'
-// May contain a valid pair of parentheses
-
-// Valid parentheses substrings:
-// (()) = complex substring, ()() = simple substrings
-
-// We assume that every new possible substring is complex
-
-// complexBracesLength should verify brackets and return 0 if they're not valid
+// entry function will calculate result from subLengths
+// subLength function will recursively calculate subLength (e.g. '(())', '()', '(((())')
 
 func longestValidParentheses(s string) int {
 	var result = 0
-
-	var n = len(s)
 	var length = 0
-	for i := 0; i < n - 1; i++ {
-		if s[i] == '(' {
-			var subLength = complexBracesLength(s, i, n)
-			if subLength > 0 {
-				length += subLength
-				i += subLength - 1
-			} else {
-				result = max(result, length)
-				length = 0
-				continue
-			}
-		} else {
-			continue
-		}
-
-		if (i < n - 1) && (s[i + 1] != '(') {
-			result = max(result, length)
-			length = 0
-			i++
-		}
-	}
-
-	if length > result {
-		result = length
-	}
-
-	return result
-}
-
-func complexBracesLength(s string, i int, n int) int {
-	var length = 0
-	var count = 0
-
-	// Step 1
-	if s[i] == '(' {
-		count++
-		for ; (i < n - 1) && s[i + 1] != ')'; i++ {
-			count++
-		}
-	} else {
-		return 0
-	}
-
-	// Step 2
-	for j := 0; j < count; j++ {
-		if (i < n - 1) && (s[i + 1] != '(') {
-			length += 2
-			i++
-		} else {
-			// Parentheses are not valid
-			return 0
-		}
-	}
-
-	return length
-}
-
-func entry(s string) int {
-	var result = 0
-	var length = 0
-	var shift = 0
 
 	var n = len(s)
 	for i := 0; i < n; i++ {
-		for ; i < n && s[i] == '('; shift++ {
-			length++
+		for ; i < n && s[i] == '('; {
 			var subLength, subShift = subLength(s, i, n)
-			length += subLength; shift += subShift
-			i += subShift
 
-			var hasPair = (i - length > 0 && i < n) && (s[i - length] == '(' && s[i] == ')')
-			if !hasPair {
-				length--
+			var hasPair = (i + subShift < n) && (s[i + subShift] == ')')
+			if (subLength != subShift) && !hasPair {
+				length = max(length, subLength)
+			} else {
+				length += subLength
 			}
+
+			i += subShift
 		}
 
 		result = max(result, length)
-		length = 0; shift = 0
+		length = 0
 	}
 
 	return result
 }
 
-func subLength(s string, i int, n int) (int, int) {
-	var lenResult = 0
-	var shiftResult = i
-
+func subLength(s string, i, n int) (int, int) {
 	var length = 0
-	for i < n - 1 {
-		if (s[i] == '(') && (s[i + 1] != ')') {
-			i++
-			var subLength, shift = subLength(s, i, n)
-			length += subLength
-			i += shift
-		} else {
-			i++
-			lenResult = 2; shiftResult = 2
-			return lenResult, shiftResult
-		}
+	var shift = i
 
-		i++
-		lenResult = max(lenResult, length)
-		length = 0
+	// may be redundant
+	if !(i < n - 1) {
+		return length, 1
 	}
 
-	shiftResult = i - shiftResult
-	return lenResult, shiftResult
+	if (s[i] == '(') && (s[i + 1] != ')') {
+		length++; i++
+		var subLength, subShift = subLength(s, i, n)
+		length += subLength
+		i += subShift
+	} else {
+		length = 2; shift = 2
+		return length, shift
+	}
+
+	var hasPair = (i - length > 0 && i < n) && (s[i - length] == '(' && s[i] == ')')
+	if !hasPair {
+		length--
+	} else {
+		length++; i++
+	}
+
+	shift = i - shift
+	return length, shift
 }
 
 // Helpers
 
-func max(a int, b int) int {
+func max(a, b int) int {
 	if a > b {
 		return a
 	}
@@ -138,12 +71,12 @@ func max(a int, b int) int {
 }
 
 func main() {
-	fmt.Println(entry("((()") == 2)
-	fmt.Println(entry(")()())") == 4)
-	fmt.Println(entry("())(())()()))") == 8)
+	fmt.Println(longestValidParentheses("((()") == 2)
+	fmt.Println(longestValidParentheses(")()())") == 4)
+	fmt.Println(longestValidParentheses("())(())()()))") == 8)
 	fmt.Println(longestValidParentheses("(") == 0)
 	fmt.Println(longestValidParentheses(")(") == 0)
 	fmt.Println(longestValidParentheses("()(())") == 6)
 	fmt.Println(longestValidParentheses("()(()") == 2)
-	fmt.Println(longestValidParentheses("(()(()())()())") == 14) // Use recursion
+	fmt.Println(longestValidParentheses("(()(()())()())") == 14)
 }
